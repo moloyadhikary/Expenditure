a76ddc6be1ca19877f85ed78067daf3d2b0196e9
Moloy Adhikary <moloy_a@hotmail.com>
2021-06-23 23:47:40 +0600

    20210623

---
+++ WebUi/Controllers/ConfigurationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessObject;
+
+namespace WebUi.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        private ExpenditureBookDbEntities db;
+
+        public ConfigurationController()
+        {
+            db = new ExpenditureBookDbEntities();
+        }
+
+
+        public ActionResult GetSubTypes()
+        {
+            var subTypes = db.ItemSubTypes.OrderBy(x=>x.Name).ToList();
+            return View(subTypes);
+        }
+
+        public ActionResult AddSubType()
+        {
+            return View();
+        }
+    }
+}
