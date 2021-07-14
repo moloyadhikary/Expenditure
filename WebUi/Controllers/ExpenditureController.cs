31142da4b592d38bef7a42714ca7052988b81089
Moloy Adhikary <moloy_a@hotmail.com>
2021-07-14 23:32:49 +0600

    20210714

---
+++ WebUi/Controllers/ExpenditureController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUi.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        private ExpenditureBookDbEntities db;
+
+        public ExpenditureController()
+        {
+            db = new ExpenditureBookDbEntities();
+        }
+       
+        public ActionResult Create()
+        {
+            var itemTypes = db.ItemTypes.ToList();
+            itemTypes.Add(new ItemType{Id = 0, Name = "Select Type"});
+            ViewBag.ItemTypeList = new SelectList(itemTypes.OrderBy(d=>d.Id), "Id", "Name");
+            
+            return View();
+        }
+        
+        [HttpPost]
+        public JsonResult GetSubTypesByType(int typeId)
+        {
+            var subTypes = db.ItemSubTypes.Where(s => s.ItemTypeId == typeId).ToList();
+            return Json(subTypes.Select(x => new
+            {
+                x.Id,
+                x.Name
+            }));
+        }
+    }
+}
---
+++ WebUi/Models/InputForms/AddExpenditureInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUi.Models.InputForms
+{
+    public class AddExpenditureInputModel
+    {
+        public int ItemTypeId { get; set; }
+        public int ItemSubTypeId { get; set; }
+        public decimal Amount { get; set; }
+        public string Remarks { get; set; }
+    }
+}
