fdf0cf19413fb71a591adaf8df68454bc510288b
Moloy Adhikary <moloy_a@hotmail.com>
2021-07-28 23:35:10 +0600

    20210728

--- WebUi/Controllers/HomeController.cs
+++ WebUi/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessObject;
 
 namespace WebUi.Controllers
 {
     public class HomeController : Controller
     {
+        private ExpenditureBookDbEntities db;
+
+        public HomeController()
+        {
+            db = new ExpenditureBookDbEntities();
+        }
+        
         public ActionResult Index()
         {
-            return View();
+            var now = DateTime.Now;
+            var startDate = new DateTime(now.Year, now.Month, 1);
+            var endDate = startDate.AddMonths(1).AddDays(-1);
+            
+            var data = db.DataEntries.Where(x=>x.IsDeleted==false && x.DataDate >= startDate && x.DataDate <= endDate).ToList();
+            return View(data);
         }
 
         public ActionResult About()
